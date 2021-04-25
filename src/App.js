dfab4438b5980caa5c7af3d2dbd9e699c583518b
Raymundo <aslan_ray@icloud.com>
2021-04-25 14:30:24 -0700

    React autocomplete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactAutocomplete from 'react-autocomplete';
 
 function App() {
+  const [inputValue, setInputValue] = useState();
+
   return (
-    <div>
-      Hello World
-    </div>
+    <ReactAutocomplete
+      items={[
+        { id: 'foo', label: 'foo' },
+        { id: 'bar', label: 'bar' },
+        { id: 'baz', label: 'baz' },
+      ]}
+      shouldItemRender={(item, value) => item.label.toLowerCase()
+        .indexOf(value.toLowerCase()) > -1}
+      getItemValue={(item) => item.label}
+      renderItem={(item, highlighted) => (
+        <div
+          key={item.id}
+          style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}
+        >
+          {item.label}
+        </div>
+      )}
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      onSelect={(value) => setInputValue(value)}
+    />
   );
 }
 
