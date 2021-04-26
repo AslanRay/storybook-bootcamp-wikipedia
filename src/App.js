15816a54426656575cd91716d941d84defcd679e
Raymundo <aslan_ray@icloud.com>
2021-04-26 14:47:43 -0700

    Axios installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactAutocomplete from 'react-autocomplete';
+import axios from 'axios';
 
 function App() {
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue] = useState('');
+  const [responseItems, setResponseItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://www.mediawiki.org/w/api.php?origin=*&action=opensearch&search=${inputValue}`)
+      .then((response) => {
+        const parsedResponse = [];
+        for (let i = 0; i < response.data[1].length; i += 1) {
+          parsedResponse.push({
+            id: response.data[3][i],
+            label: response.data[1][i],
+          });
+        }
+        setResponseItems(parsedResponse);
+      });
+  }, [inputValue]);
 
   return (
     <ReactAutocomplete
-      items={[
-        { id: 'foo', label: 'foo' },
-        { id: 'bar', label: 'bar' },
-        { id: 'baz', label: 'baz' },
-      ]}
+      items={responseItems}
       shouldItemRender={(item, value) => item.label.toLowerCase()
         .indexOf(value.toLowerCase()) > -1}
       getItemValue={(item) => item.label}
